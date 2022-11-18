8be1f1b76e2c96c2d73d55a898d66c8b72ae97bb
Julien Robert <julien.robert.info@gmail.com>
2022-11-18 23:55:04 +0100

    networks data

---
+++ src/data/networks.ts
@@ -0,0 +1,75 @@
+interface ChainInfo {
+  chainName: string
+  nativeCurrency: {
+    name: string
+    symbol: string
+    decimals: number
+  }
+  rpcUrls: string
+  blockExplorerUrls: string
+  blockExplorerApiUrls?: string
+  blockExplorerApiToken?: string
+}
+
+export const chains: { [chainId: number]: ChainInfo } = {
+  1: {
+    chainName: 'Ethereum',
+    nativeCurrency: {
+      name: 'Ether',
+      symbol: 'ETH',
+      decimals: 18
+    },
+    rpcUrls: 'https://rpc.ankr.com/eth',
+    blockExplorerUrls: 'https://etherscan.io',
+    blockExplorerApiUrls: 'https://api.etherscan.io/api',
+    blockExplorerApiToken: process.env.NEXT_PUBLIC_ETHEREUM_API_TOKEN
+  },
+  56: {
+    chainName: 'BSC',
+    nativeCurrency: {
+      name: 'Binance Coin',
+      symbol: 'BNB',
+      decimals: 18
+    },
+    rpcUrls: 'https://bsc-dataseed.binance.org',
+    blockExplorerUrls: 'https://bscscan.com/',
+    blockExplorerApiUrls: 'https://api.bscscan.com/api',
+    blockExplorerApiToken: process.env.NEXT_PUBLIC_BSC_API_TOKEN
+  },
+  137: {
+    chainName: 'Polygon',
+    nativeCurrency: {
+      name: 'MATIC',
+      symbol: 'MATIC',
+      decimals: 18
+    },
+    rpcUrls: 'https://polygon-rpc.com',
+    blockExplorerUrls: 'https://polygonscan.com/',
+    blockExplorerApiUrls: 'https://api.polygonscan.com/api',
+    blockExplorerApiToken: process.env.NEXT_PUBLIC_POLYGON_API_TOKEN
+  },
+  250: {
+    chainName: 'Fantom',
+    nativeCurrency: {
+      name: 'FTM',
+      symbol: 'FTM',
+      decimals: 18
+    },
+    rpcUrls: 'https://rpc.ftm.tools',
+    blockExplorerUrls: 'https://ftmscan.com/',
+    blockExplorerApiUrls: 'https://api.ftmscan.com/api',
+    blockExplorerApiToken: process.env.NEXT_PUBLIC_FANTOM_API_TOKEN
+  },
+  100: {
+    chainName: 'Gnosis',
+    nativeCurrency: {
+      name: 'xDai',
+      symbol: 'xDai',
+      decimals: 18
+    },
+    rpcUrls: 'https://rpc.xdaichain.com/',
+    blockExplorerUrls: 'https://blockscout.com/poa/xdai',
+    blockExplorerApiUrls: 'https://api.polygonscan.com/api',
+    blockExplorerApiToken: process.env.NEXT_PUBLIC_GNOSIS_API_TOKEN
+  }
+}
